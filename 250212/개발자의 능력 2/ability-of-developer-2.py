a21edfc253e990f015762b4f84268807d2187c72
박성열(SungYul Park) <114918431+SuungYul@users.noreply.github.com>
2025-02-12 21:55:00 +0900

    Title: 개발자의 능력 2, Time: 74ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250212/개발자의 능력 2/ability-of-developer-2.py
@@ -0,0 +1,26 @@
+ability = list(map(int, input().split()))
+
+# Write your code here!
+min_diff = 1000000
+
+def get_diff(i, j):
+    sum1 = ability[i] + ability[j]
+    md = 1000000
+    for k in range(5):
+        for m in range(k+1,6):
+            if i!=k and i!=m and j!=k and j!=m:
+                if md > abs(ability[k] + ability[m] - sum1):
+                    md = abs(ability[k] + ability[m] - sum1)
+                    sum2 = ability[k] + ability[m]
+
+    
+    sum3 = sum(ability) - sum1 - sum2
+
+    return max(sum1, sum2, sum3) - min(sum1, sum2, sum3)
+
+
+for i in range(5):
+    for j in range(i+1, 6):
+        min_diff = min(min_diff, get_diff(i, j))
+
+print(min_diff)
