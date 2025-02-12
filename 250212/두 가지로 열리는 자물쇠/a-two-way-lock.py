5e738d087d533b0a052f91e65f8e2df924551fb4
박성열(SungYul Park) <114918431+SuungYul@users.noreply.github.com>
2025-02-12 18:06:42 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 79ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250212/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -0,0 +1,38 @@
+N = int(input())
+a1, b1, c1 = map(int, input().split())
+a2, b2, c2 = map(int, input().split())
+
+# Write your code here!
+l = [i for i in range(1, N+1)]
+
+l_a1 = list()
+l_b1 = list()
+l_c1 = list()
+
+l_a2 = list()
+l_b2 = list()
+l_c2 = list()
+
+
+for i in range(-2, 3):
+    l_a1.append(l[a1-i])
+    l_b1.append(l[b1-i])
+    l_c1.append(l[c1-i])
+
+    l_a2.append(l[a2-i])
+    l_b2.append(l[b2-i])
+    l_c2.append(l[c2-i])
+
+s = set()
+
+for i in l_a1:
+    for j in l_b1:
+        for k in l_c1:
+            s.add((i,j,k))
+
+for i in l_a2:
+    for j in l_b2:
+        for k in l_c2:
+            s.add((i,j,k))
+
+print(len(s))
