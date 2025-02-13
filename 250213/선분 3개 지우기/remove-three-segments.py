6d57014d5845d7171d9652c2bd37c875a39300b4
박성열(SungYul Park) <114918431+SuungYul@users.noreply.github.com>
2025-02-13 21:18:36 +0900

    Title: 선분 3개 지우기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250213/선분 3개 지우기/remove-three-segments.py
@@ -0,0 +1,28 @@
+n = int(input())
+arr = [tuple(map(int, input().split())) for _ in range(n)]
+# Write your code here!
+
+def f(l):
+    for i in range(len(l)):
+        a, b = l[i]
+        for j in range(i+1, len(l)):
+            c,d = l[j]
+            if b >= c :
+                return False
+    
+    return True
+
+cnt = 0
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            tmp = list(arr)
+            del tmp[k]
+            del tmp[j]
+            del tmp[i]
+            
+            if f(tmp):
+                cnt+=1
+
+print(cnt)
+
