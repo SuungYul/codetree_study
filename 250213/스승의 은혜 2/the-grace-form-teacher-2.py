bca5e7dfcec05968897dd773725fca6e940a9867
박성열(SungYul Park) <114918431+SuungYul@users.noreply.github.com>
2025-02-13 17:32:11 +0900

    Title: 스승의 은혜 2, Time: 103ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250213/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,22 @@
+N, B = map(int, input().split())
+P = [int(input()) for _ in range(N)]
+
+# Write your code here!
+max_p = [0]* B
+
+P.sort(reverse=True)
+
+for i in range(N):
+    sum_p = P[i] // 2
+    cnt = 1
+    bound = B - sum_p
+    for j in range(i+1, N):
+        if P[j] > bound:
+            break
+        bound -= P[j]
+        cnt+=1
+        sum_p+=P[j]
+    
+    max_p[sum_p-1] = cnt
+
+print(max(max_p))
