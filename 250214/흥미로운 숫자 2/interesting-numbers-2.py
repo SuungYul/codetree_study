f7be51fc0dac6b5ddac4ddbfb7a9efbbbfbc65dc
박성열(SungYul Park) <114918431+SuungYul@users.noreply.github.com>
2025-02-14 19:56:44 +0900

    Title: 흥미로운 숫자 2, Time: 253ms, Memory: 19MB, Status: Passed - CodeTree

--- 250214/흥미로운 숫자 2/interesting-numbers-2.py
+++ 250214/흥미로운 숫자 2/interesting-numbers-2.py
@@ -5,10 +5,21 @@ cnt = 0
 
 for i in range(X, Y+1):
     l = list(map(int, str(i)))
-    s = set()
-    for j in l:
-        s.add(j)
-    if len(s) == 2:
-        cnt+=1
+    s = set(map(int, str(i)))
+    if len(s) == 2:    
+        d = dict()
+        for j in s:
+            d[j] = 0
+        
+        for j in l:
+            d[j]+=1
+        
+        a, b = d.values()
+        
+        if (a>1 and b == 1) or (a == 1 and b>1):
+            cnt+=1
+        
+    
+
 
 print(cnt)
