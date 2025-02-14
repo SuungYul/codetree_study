4e66a59383fc62a1562217e48cd4a38eef7b596c
박성열(SungYul Park) <114918431+SuungYul@users.noreply.github.com>
2025-02-14 17:36:17 +0900

    Title: 스승의 은혜 3, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250214/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250214/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,16 +1,17 @@
 N, B = map(int, input().split())
 gifts = [tuple(map(int, input().split())) for _ in range(N)]
+gifts.sort(key=lambda x: (x[0]+x[1]))
 P = [gift[0] for gift in gifts]
 S = [gift[1] for gift in gifts]
 
 # Write your code here!
-max_p = [0]*B
+max_cnt = 0
 
 for i in range(N):
     if P[i] // 2 + S[i] > B:
         continue
-    sum_p = P[i] // 2 + S[i]
-    bound = B - sum_p
+    # sum_p = P[i] // 2 + S[i]
+    bound = B - P[i] // 2 - S[i]
     cnt = 1
     for j in range(N):
         if i == j:
@@ -18,9 +19,9 @@ for i in range(N):
         if P[j] + S[j] > bound:
             continue
         bound = bound - (P[j] + S[j])
-        sum_p += P[j] + S[j]
+        # sum_p += P[j] + S[j]
         cnt+=1
     
-    max_p[sum_p -1] = max(max_p[sum_p-1], cnt)
+    max_cnt = max(max_cnt, cnt)
 
-print(max(max_p))
+print(max_cnt)
