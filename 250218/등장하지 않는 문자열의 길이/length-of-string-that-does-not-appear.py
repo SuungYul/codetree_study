cbd0341032c666aec08db1bf3f7e85547d80a95b
박성열(SungYul Park) <114918431+SuungYul@users.noreply.github.com>
2025-02-18 23:15:42 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250218/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 250218/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -1,15 +1,23 @@
-N = int(input())
-str = input()
+n = int(input())
+s = input()
 
-# Write your code here!
+ans = 0
 
-cnt = 0
-for i in range(N):
-    for j in range(i+1, N):   
-        s = str[i:j]
-        if s in str[j:]:
-            cnt = max(cnt,len(s))
-        else:
+for i in range(1, n):  # 길이 i의 부분 문자열 검사
+    seen = set()  # 중복 확인을 위해 `set` 사용
+    success = True  
+
+    for j in range(n - i + 1):  # 모든 부분 문자열 검사
+        tmp = s[j:j+i]
+        
+        if tmp in seen:  # 중복 존재하면 실패
+            success = False
             break
+        
+        seen.add(tmp)  # 새로운 부분 문자열 저장
 
-print(cnt+1)
+    if success:  # 중복이 없는 첫 번째 길이 찾으면 종료
+        ans = i
+        break
+
+print(ans)
