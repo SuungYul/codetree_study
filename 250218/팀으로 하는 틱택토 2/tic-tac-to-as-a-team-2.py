9025f407522dff285a7b0451fa519acc769a56c7
박성열(SungYul Park) <114918431+SuungYul@users.noreply.github.com>
2025-02-18 22:08:04 +0900

    Title: 팀으로 하는 틱택토 2, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250218/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 250218/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -2,21 +2,36 @@ inp = [list(map(int,input())) for _ in range(3)]
 
 # Write your code here!
 cnt = 0
+already = list()
 for i in range(3):
     s = set(inp[i])
     if len(s) == 2:
-        cnt+=1
+        tmp = sorted(s)
+        if tmp not in already:
+            cnt+=1
+            already.append(tmp)
+        
+
 
 for i in range(3):
     s = set([inp[0][i], inp[1][i], inp[2][i]])
     if len(s) == 2:
-        cnt+=1
-    
+        tmp = sorted(s)
+        if tmp not in already:
+            cnt+=1
+        already.append(tmp)
+
 s = set([inp[0][0], inp[1][1], inp[2][2]])
 if len(s) == 2:
-    cnt+=1
+    tmp = sorted(s)
+    if tmp not in already:
+        cnt+=1
+    already.append(tmp)
 s = set([inp[2][0], inp[1][1], inp[0][2]])
 if len(s) == 2:
-    cnt+=1
+    tmp = sorted(s)
+    if tmp not in already:
+        cnt+=1
+    already.append(tmp)
 
 print(cnt)
