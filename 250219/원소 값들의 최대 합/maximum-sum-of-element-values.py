361541b9be27ca94c1e77ed97c1fbf0d79e64962
박성열(SungYul Park) <114918431+SuungYul@users.noreply.github.com>
2025-02-19 22:32:13 +0900

    Title: 원소 값들의 최대 합, Time: 55ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250219/원소 값들의 최대 합/maximum-sum-of-element-values.py
+++ 250219/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -1,18 +1,21 @@
 n, m = map(int, input().split())
-a = [0] + list(map(int, input().split()))
+a = [0] + list(map(int, input().split()))  # 1-based index 적용
 
-# Write your code here!
 max_sum = 0
-for i in range(1, n+1): # 시작위치
+
+for i in range(1, n+1):  # 시작 위치
     res = 0
-    arr = list(a)
-    start = arr[i]
+    visited = set()  # 방문한 위치 저장
+    start = i  # 시작값 설정
+
     for j in range(m):
-        temp = arr[start]
-        arr[start] = start
-        res += start
-        start = temp
-        
-    max_sum = max(max_sum, res)
+        if start in visited:  # 이미 방문한 곳이면 탈출 (사이클 감지)
+            break
+        visited.add(start)
+
+        res += start  # 현재 위치 값 누적
+        start = a[start]  # 다음 이동 위치
+
+    max_sum = max(max_sum, res)  # 최대값 갱신
 
 print(max_sum)
