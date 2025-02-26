825eae7482827700da4f4ab3eb28a6b8380eb958
박성열(SungYul Park) <114918431+SuungYul@users.noreply.github.com>
2025-02-26 18:36:45 +0900

    Title: 최대 최소간의 차, Time: 332ms, Memory: 17MB, Status: Passed - CodeTree

--- 250226/최대 최소간의 차/difference-between-maximum-and-minimum.py
+++ 250226/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -2,8 +2,8 @@ n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
-min_cost = 10000
-for a in range(1, 10001):
+min_cost = 10000 * 100
+for a in range(1, 10000 * 100 +1):
     cost = 0
     for elem in arr:
         if a <= elem and elem <= a + k:
