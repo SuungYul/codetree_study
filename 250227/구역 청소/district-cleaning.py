556cbb916aa9219d8b1d2ef21eb4c55af51ba5cd
박성열(SungYul Park) <114918431+SuungYul@users.noreply.github.com>
2025-02-27 15:04:00 +0900

    Title: 구역 청소, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 250227/구역 청소/district-cleaning.py
+++ 250227/구역 청소/district-cleaning.py
@@ -2,7 +2,7 @@ a, b = map(int, input().split())
 c, d = map(int, input().split())
 
 # Please write your code here.
-if a < d or b > c :
-    print(max(a,b,c,d) - min(a,b,c,d))
-else:
+if a > d or b < c :
     print(b-a + d-c)
+else:
+    print(max(a,b,c,d) - min(a,b,c,d))
