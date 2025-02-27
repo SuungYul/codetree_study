916b3338d93816276b7a7b8692710296ebc242a2
박성열(SungYul Park) <114918431+SuungYul@users.noreply.github.com>
2025-02-27 16:16:02 +0900

    Title: 전부 겹치는 선분 2, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 250227/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 250227/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -11,7 +11,7 @@ for i in range(n):
     x = [t[0] for t in tmp]
     y = [t[1] for t in tmp]
     
-    if min(y) <= max(x):
+    if min(y) >= max(x):
         possible = True
 
 print("Yes" if possible else "No")
