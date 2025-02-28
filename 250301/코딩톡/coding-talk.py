93989d82cf4cad62bb6c2a508bb9ed83d2bea9e7
박성열(SungYul Park) <114918431+SuungYul@users.noreply.github.com>
2025-03-01 03:12:47 +0900

    Title: 코딩톡, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250301/코딩톡/coding-talk.py
+++ 250301/코딩톡/coding-talk.py
@@ -1,19 +1,27 @@
-n, m, p = map(int, input().split())
-messages = [tuple(input().split()) for _ in range(m)]
-c = [msg[0] for msg in messages]
-u = [int(msg[1]) for msg in messages]
+import sys
 
-# Please write your code here.
-un_read = [chr(i) for i in range(ord('A'), ord('A')+n)]
-# print(un_read)
-for i in range(p-1, m):
-    if i>0 and u[i-1] == u[i]:
-        if c[i-1] in un_read:
-            un_read.remove(c[i-1])
-    if u[i] == 0:
-        un_read.clear()
-    if c[i] in un_read:
-        un_read.remove(c[i])
+n, m, p = tuple(map(int, input().split()))
+message = [
+    list(input().split())
+    for _ in range(m)
+]
 
-print(*un_read)
+# 모두 읽은 채팅이라면 읽지 않은 사람은 없습니다.
+if int(message[p - 1][1]) == 0:
+    sys.exit()
 
+# 각 사람에 대해 채팅을 읽었을지 안 읽었을지 판단합니다.
+for i in range(n):
+    # read : 확실하게 채팅을 읽었으면 true
+    person = chr(ord('A') + i)
+    read = False
+
+    # 만약 p번 메시지를 읽은 사람 수와 같은 채팅을 기준으로
+    # 한번이라도 채팅을 쳤다면 확실하게 채팅을 읽었습니다.
+    for c, u in message:
+        u = int(u)
+        if u >= int(message[p - 1][1]) and c == person:
+            read = True
+    
+    if read == False:
+        print(person, end=" ")
