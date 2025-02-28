edf484d981ab511324bb320a8ee3b4c099531de8
박성열(SungYul Park) <114918431+SuungYul@users.noreply.github.com>
2025-03-01 02:53:37 +0900

    Title: 코딩톡, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250301/코딩톡/coding-talk.py
@@ -0,0 +1,14 @@
+n, m, p = map(int, input().split())
+messages = [tuple(input().split()) for _ in range(m)]
+c = [msg[0] for msg in messages]
+u = [int(msg[1]) for msg in messages]
+
+# Please write your code here.
+un_read = [chr(i) for i in range(ord('A'), ord('A')+n)]
+# print(un_read)
+for i in range(p-1, m):
+    if c[i] in un_read:
+        un_read.remove(c[i])
+
+print(*un_read)
+
