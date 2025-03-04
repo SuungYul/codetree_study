d6f31684e9729ab4cd7e24440525833b0c7caa3d
박성열(SungYul Park) <114918431+SuungYul@users.noreply.github.com>
2025-03-04 19:52:43 +0900

    Title: 독서실의 거리두기 3, Time: 67ms, Memory: 17MB, Status: Passed - CodeTree

--- 250304/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 250304/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -11,5 +11,12 @@ for i in range(N):
                 close_d = min(close_d, abs(i-j))
         
         max_d = max(max_d, close_d)
+    
+already_close = N
+for i in range(N):
+    for j in range(i+1, N):
+        if seats[i] == '1' and seats[j] == '1':
+            already_close = min(already_close, j-i)
 
-print(max_d)
+
+print(min(max_d, already_close))
