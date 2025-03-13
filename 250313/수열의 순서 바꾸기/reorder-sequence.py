6ba4617fec386858ccd897378bd2ab412cca7c47
박성열(SungYul Park) <114918431+SuungYul@users.noreply.github.com>
2025-03-13 22:45:35 +0900

    Title: 수열의 순서 바꾸기, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250313/수열의 순서 바꾸기/reorder-sequence.py
+++ 250313/수열의 순서 바꾸기/reorder-sequence.py
@@ -3,7 +3,7 @@ sequence = list(map(int, input().split()))
 
 # Please write your code here.
 
-for cnt in range(n-1, 1, -1):
+for cnt in range(n-1, -1, -1):
     if sequence[cnt] < sequence[cnt-1]:
         break
 
